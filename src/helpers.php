e6976f7020dbe441942ad1c301c636c9e7a20a4c
fanzewei <fanzewei@yongche.com>
2022-05-13 11:13:09 +0800

    登录日志

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Log;
+
+if (!function_exists('deep_login_info')) {
+    function deep_login_info($message, array $context = array())
+    {
+        Log::channel('deep_login_info')->info($message, $context);
+    }
+}
